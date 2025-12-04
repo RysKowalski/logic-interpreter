0be44da0af4cee41c54c2571f6b26ffbafb7fefd
RysKowalski <ryskowalski0@gmail.com>
2025-12-04 12:31:54 +0100

    up

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from typing import Literal, TypedDict
 
 
-def load_file(path: str) -> list[str]:
-    with open(path, 'r'):
+class Operation(TypedDict):
+    # 0: flow_start, 1: flow_stop
+    code: int
+    operands: str
 
+
+class Program(TypedDict):
+    jumps: list[int]
+    vars: list[int]
+    operations: list[Operation]
+
+
+def load_file(path: str = "./example.loi") -> list[str]:
+    with open(path, "r") as file:
+        return file.readlines()
+
+
+if __name__ == "__main__":
+    print(load_file())
