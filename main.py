4709fa9a2f01de21619ba8ecb211fe17aac2fc2e
RysKowalski <ryskowalski0@gmail.com>
2025-12-05 08:01:51 +0100

    starting writing some code ig, i dont have time to write

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-from typing import Literal, TypedDict
+class Operation:
+    def __init__(self, code: int, operands: str) -> None:
+        """0: flow_start, 1: flow_stop"""
+        self.code: int = code
+        self.operands: str = operands
 
 
-class Operation(TypedDict):
-    # 0: flow_start, 1: flow_stop
-    code: int
-    operands: str
+class Program:
+    def __init__(self, raw_program: list[str]) -> None:
+        self.current_index: int = 0
 
+        self.jumps: list[int] = []
+        self.vars: list[int] = []
+        self.operations: list[Operation] = []
 
-class Program(TypedDict):
-    jumps: list[int]
-    vars: list[int]
-    operations: list[Operation]
+        for line in raw_program:
+            split_line: list[str] = line.split(" ")
+            match split_line[0]:
+                case "FLOW":
+                    match split_line[1]:
+                        case "START":
+                            self.operations.append(Operation(0, ""))
+                        case "END":
+                            self.operations.append(Operation(1, ""))
 
 
 def load_file(path: str = "./example.loi") -> list[str]:
     with open(path, "r") as file:
-        return file.readlines()
+        return [line.rstrip("\n") for line in file]
 
 
 if __name__ == "__main__":
-    print(load_file())
+    file = load_file()
+    prog = Program(file)
+    print(
+        prog.current_index,
+        [op.code for op in prog.operations] + [op.operands for op in prog.operations],
+        prog.jumps,
+        prog.vars,
+        sep="\n",
+    )
